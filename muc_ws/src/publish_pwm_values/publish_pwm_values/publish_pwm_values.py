46ef68c3acd6c064f939e7302fae8c0592f768be
anirudh <agrawalanirudh18@gmail.com>
2024-02-10 23:41:32 +0530

    Updated publish_pwm.py

--- muc_ws/src/publish_pwm_values/publish_pwm_values/publish_pwm_values.py
+++ muc_ws/src/publish_pwm_values/publish_pwm_values/publish_pwm_values.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 
-import sys
 import rclpy
 from rclpy.node import Node
+from geometry_msgs.msg import Twist
 from std_msgs.msg import Int32
 
 class PublishPWMValues(Node):
@@ -10,20 +10,31 @@ class PublishPWMValues(Node):
         super().__init__('publish_pwm_values')
         self.publisher1 = self.create_publisher(Int32, 'pwm_values_1', 10)
         self.publisher2 = self.create_publisher(Int32, 'pwm_values_2', 10)
-        self.pwm_value1 = int(sys.argv[1]) if len(sys.argv) > 1 else 0
-        self.pwm_value2 = int(sys.argv[2]) if len(sys.argv) > 2 else 0
-        self.timer = self.create_timer(1.0, self.publish_pwm)
+        self.subscription = self.create_subscription(
+            Twist,
+            'cmd_vel',
+            self.cmd_vel_callback,
+            10
+        )
 
-    def publish_pwm(self):
+    def cmd_vel_callback(self, msg):
+        linear_x = msg.linear.x
+        angular_z = msg.angular.z
+
+        # Calculate PWM values for both motors
+        pwm_value1 = int(linear_x * 100 + angular_z * 100)  # Example calculation for motor 1
+        pwm_value2 = int(linear_x * 100 - angular_z * 100)  # Example calculation for motor 2
+
+        # Publish PWM values for each motor
         msg1 = Int32()
-        msg1.data = self.pwm_value1
+        msg1.data = pwm_value1
         self.publisher1.publish(msg1)
-        self.get_logger().info('Publishing PWM value 1: %d' % self.pwm_value1)
 
         msg2 = Int32()
-        msg2.data = self.pwm_value2
+        msg2.data = pwm_value2
         self.publisher2.publish(msg2)
-        self.get_logger().info('Publishing PWM value 2: %d' % self.pwm_value2)
+
+        self.get_logger().info('Publishing PWM values - Motor 1: %d, Motor 2: %d' % (pwm_value1, pwm_value2))
 
 def main(args=None):
     rclpy.init(args=args)
@@ -34,3 +45,4 @@ def main(args=None):
 
 if __name__ == '__main__':
     main()
+
